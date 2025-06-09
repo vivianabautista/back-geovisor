15606d98d85296dcc418a0d63dccefd7418e7fd4
viviana <lviviana13@gmail.com>
2025-06-08 21:58:53 -0500

    feat: :sparkles: services to section-question

--- polls/serializers.py
+++ polls/serializers.py
@@ -1,20 +1,11 @@
 from django.contrib.auth.models import Group, User
 from rest_framework import serializers
 
-from .models import Form, Section, Question, TypeChoice
+from .models import *
 
-class FormSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Form
-        fields = '__all__'
-        depth = 1
-        
-    
-class SectionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Section
-        fields = '__all__'
-        depth = 1
+
+
+
 
 class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
@@ -27,8 +18,52 @@ class QuestionSerializer(serializers.ModelSerializer):
 
         }
 
+class SectionListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Section
+        fields = '__all__'
+        depth = 1
+
+
+class SectionDetailSerializer(serializers.ModelSerializer):
+    questions = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Section
+        fields = '__all__'
+        depth = 1
+
+    def get_questions(self, obj):
+        # Get questions through the SectionQuestion model
+        section_questions = SectionQuestion.objects.filter(section=obj)
+        questions = Question.objects.filter(sectionquestion__section=obj)
+        return QuestionSerializer(questions, many=True).data
+
+        
+class SectionQuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SectionQuestion
+        fields = ['id', 'section', 'question']
+
+
 class TypeChoiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = TypeChoice
         fields = '__all__'
         depth = 1
+
+
+class FormSerializer(serializers.ModelSerializer):
+    sections = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Form
+        fields = '__all__'
+        depth = 1
+
+    def get_sections(self, obj):
+        sections = []
+        form_sections = FormSection.objects.filter(form=obj)
+        for form_section in form_sections:
+            sections.append(SectionListSerializer(form_section.section).data)
+        return sections
--- polls/urls.py
+++ polls/urls.py
@@ -1,13 +1,21 @@
 from django.urls import path
-from .views import FormView, SectionView, QuestionView, QuestionTypeView
+from .views import *
 
 urlpatterns = [
     path("form/", FormView.as_view()),
+    path("form/<int:pk>/", FormView.as_view(), name='form-detail'),
     path("form/<str:search>/", FormView.as_view()),
+    
+
+    path("form/<int:form_id>/section/<int:section_id>/", FormSectionView.as_view(), name='form-section'),
+   
+    path("section/<int:pk>/", SectionView.as_view()),
     path("section/<str:search>/", SectionView.as_view()),
     path("section/", SectionView.as_view()),
+
     path("question/<str:search>/", QuestionView.as_view()),
     path("question/", QuestionView.as_view()),
-    path("question-type/", QuestionTypeView.as_view())
-    
+    path("question-type/", QuestionTypeView.as_view()),
+
+    path("section/<int:section_id>/question/<int:question_id>/", SectionQuestionView.as_view(), name='section-question')
 ]
